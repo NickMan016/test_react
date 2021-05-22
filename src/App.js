2674f6c999b96924c10363701834e334ac4ff833
Nicholas Pérez <nperez@dannyyesoft.mx>
2021-05-21 20:19:11 -0500

    20210521_Adaptación de Proyecto para inicializar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Mi Primera App con React
-                </p>
-            </header>
+        <div className="font-roboto">
+            <h1>Hola Mundo</h1>
         </div>
     );
 }
